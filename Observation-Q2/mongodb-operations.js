9e133adf88571a111a1378c9f8bc2beced80334a
Princywilson <161227175+Princywilson@users.noreply.github.com>
2025-03-12 00:04:39 +0530

    Update mongodb-operations.js

--- Observation-Q2/mongodb-operations.js
+++ Observation-Q2/mongodb-operations.js
@@ -223,3 +223,76 @@ db.accounts.aggregate([
   },
   { $sort: { total_balance: -1 } }
 ])
+
+
+/*
+-- MongoDB Query Results
+-- ----------------------------------------------------------------
+// Find customers with both account types
+{
+  "_id": 1,
+  "cname": "John Smith"
+},
+{
+  "_id": 2,
+  "cname": "Mary Johnson"
+},
+{
+  "_id": 3,
+  "cname": "Robert Brown"
+}
+
+// Find branches with the most accounts
+{
+  "_id": "B002",
+  "account_count": 3,
+  "total_balance": 42000.00
+},
+{
+  "_id": "B001",
+  "account_count": 2,
+  "total_balance": 37000.00
+},
+{
+  "_id": "B003",
+  "account_count": 2,
+  "total_balance": 52000.00
+},
+{
+  "_id": "B004",
+  "account_count": 1,
+  "total_balance": 8000.00
+}
+
+// Customer balance summary
+{
+  "customer_id": 2,
+  "customer_name": "Mary Johnson",
+  "total_balance": 45000.00,
+  "account_count": 2
+},
+{
+  "customer_id": 1,
+  "customer_name": "John Smith",
+  "total_balance": 37000.00,
+  "account_count": 2
+},
+{
+  "customer_id": 4,
+  "customer_name": "Patricia Davis",
+  "total_balance": 22000.00,
+  "account_count": 1
+},
+{
+  "customer_id": 3,
+  "customer_name": "Robert Brown",
+  "total_balance": 27000.00,
+  "account_count": 2
+},
+{
+  "customer_id": 5,
+  "customer_name": "Michael Wilson",
+  "total_balance": 8000.00,
+  "account_count": 1
+}
+*/
