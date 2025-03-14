4c760f64357213dc924ab5e04919821874634f33
Princywilson <161227175+Princywilson@users.noreply.github.com>
2025-03-15 01:21:07 +0530

    Update mongodb-operations.js

--- Observation-Q2/mongodb-operations.js
+++ Observation-Q2/mongodb-operations.js
@@ -1,24 +1,52 @@
 // Assuming you have a MongoDB connection established and databases named 'customerDB'
 
 // --- CREATE Operations ---
+db.createCollection("customer")
+db.createCollection("account")
 
 // Insert a new customer into the CUSTOMER collection
-db.customer.insertOne({
-  CID: 6,
+db.customer.insertOne(
+   {
+  CID: 1,
+  CNAME: "John Smith"
+}
+ {
+  CID: 2,
+  CNAME: "Mary Johnson"
+}
+{
+  CID: 3,
   CNAME: "David Lee"
-});
+}
+);
 
 // Output/Result:
 // { "acknowledged" : true, "insertedId" : ObjectId("...") }
 
 // Insert a new account into the ACCOUNT collection
-db.account.insertOne({
-  ANO: "A009",
+db.account.insertOne(
+  {
+  ANO: "A001",
+  ATYPE: "C",
+  BALANCE: 20000,
+  CID: 1,
+  BCODE: "B001"
+},
+ {
+  ANO: "A002",
+  ATYPE: "S",
+  BALANCE: 50000,
+  CID: 2,
+  BCODE: "B002"
+},
+  {
+  ANO: "A003",
   ATYPE: "C",
   BALANCE: 10000,
-  CID: 6,
-  BCODE: "B005"
-});
+  CID: 3,
+  BCODE: "B003"
+}
+);
 
 // Output/Result:
 // { "acknowledged" : true, "insertedId" : ObjectId("...") }
@@ -33,8 +61,7 @@ db.customer.find({});
 // [
 //   { "_id": ObjectId("..."), "CID": 1, "CNAME": "John Smith" },
 //   { "_id": ObjectId("..."), "CID": 2, "CNAME": "Mary Johnson" },
-//   // ... other customers
-//   { "_id": ObjectId("..."), "CID": 6, "CNAME": "David Lee" }
+//   { "_id": ObjectId("..."), "CID": 3, "CNAME": "David Lee" }
 // ]
 
 // Find accounts for a specific customer (CID: 2)
@@ -42,48 +69,51 @@ db.account.find({ CID: 2 });
 
 // Output/Result:
 // [
-//   { "_id": ObjectId("..."), "ANO": "A003", "ATYPE": "S", "BALANCE": 15000, "CID": 2, "BCODE": "B002" },
-//   { "_id": ObjectId("..."), "ANO": "A004", "ATYPE": "C", "BALANCE": 30000, "CID": 2, "BCODE": "B003" }
+//   { "_id": ObjectId("..."), "ANO": "A002", "ATYPE": "S", "BALANCE": 5000, "CID": 2, "BCODE": "B002" },
 // ]
 
 // Find a customer by CID
 db.customer.findOne({ CID: 3 });
 
 // Output/Result:
-// { "_id": ObjectId("..."), "CID": 3, "CNAME": "Robert Brown" }
+// { "_id": ObjectId("..."), "CID": 3, "CNAME": "David Lee" }
 
 
 // --- UPDATE Operations ---
 
 // Update a customer's name (CID: 4)
 db.customer.updateOne(
-  { CID: 4 },
+  { CID: 3 },
   { $set: { CNAME: "Patricia Williams" } }
 );
 
 // Output/Result:
 // { "acknowledged" : true, "matchedCount" : 1, "modifiedCount" : 1 }
 
-// Update the balance of an account (ANO: "A001")
-db.account.updateOne(
-  { ANO: "A001" },
-  { $inc: { BALANCE: 500 } } // Increment balance by 500
-);
+db.Employee.updateMany(
+  { DEPT_NO: "D003" },
+  { $mul: { BASIC: 1.1 } }
+)
 
-// Output/Result:
-// { "acknowledged" : true, "matchedCount" : 1, "modifiedCount" : 1 }
+// Output:
+// {
+//   "acknowledged": true,
+//   "matchedCount": 1,
+//   "modifiedCount": 1
+// }
 
 
 // --- DELETE Operations ---
 
 // Delete a customer (CID: 5)
-db.customer.deleteOne({ CID: 5 });
+db.customer.deleteOne({ CID: 3 });
 
 // Output/Result:
 // { "acknowledged" : true, "deletedCount" : 1 }
 
-// Delete an account (ANO: "A008")
-db.account.deleteOne({ ANO: "A008" });
+db.customer.deleteMany({ 
+  BASIC: { $lt: 60000 }
+})
 
 // Output/Result:
 // { "acknowledged" : true, "deletedCount" : 1 }
